561bd3048ed331e08453e1e6eb833ba54eac1aad
Elon Bar-Evan <elon01@gmail.com>
2014-06-23 14:37:53 -0600

    first commit with everything

---
+++ cookbooks/iis_demo/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: iis_demo
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+powershell_script "Install IIS" do
+	code "add-windowsfeature Web-Server"
+end
+
+service "w3svc" do
+	action [ :enable, :start ]
+end
+
+cookbook_file 'c:\inetpub\wwwroot\Default.htm' do
+	source "Default.htm"
+	rights :read, "Everyone"
+end
